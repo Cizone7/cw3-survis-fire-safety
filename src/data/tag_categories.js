64b1d6e6d0b16cb712a625e880b978e4a4a58d45
Cizone7 <chenhongbei1025@163.com>
2026-05-10 17:35:08 +0800

    Add CW3 SurVis literature browser

---
+++ src/data/tag_categories.js
@@ -0,0 +1,14 @@
+const userDefinedTagCategories = {
+    "role": {
+        "description": "Role of the paper in the review"
+    },
+    "category": {
+        "description": "Literature classification category"
+    },
+    "method": {
+        "description": "Main method or system type"
+    },
+    "sensor": {
+        "description": "Main sensor or data modality"
+    }
+};
